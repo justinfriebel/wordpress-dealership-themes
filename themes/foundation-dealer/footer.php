6d238e74fad8722e9b51d0df4310b2cbb0bcdcc8
Justin Friebel <friebj99@gmail.com>
2014-04-30 14:39:04 -0500

    footer container width

--- themes/foundation-dealer/footer.php
+++ themes/foundation-dealer/footer.php
@@ -1,11 +1,13 @@
 	</div>
 </section>
 
-<footer class="row">
-	<?php do_action('foundationPress_before_footer'); ?>
-	<?php dynamic_sidebar("footer-widgets"); ?>
-	<?php do_action('foundationPress_after_footer'); ?>
-	<p>&copy; Copyright <?php bloginfo('name'); ?>. <a href="http://ww.dealerfire.com/" target="_blank">Automotive Marketing by DEALERFIRE</a>.</p>
+<footer id="footer">
+	<div class="row">
+		<?php do_action('foundationPress_before_footer'); ?>
+		<?php dynamic_sidebar("footer-widgets"); ?>
+		<?php do_action('foundationPress_after_footer'); ?>
+		<p>&copy; Copyright <?php bloginfo('name'); ?>. <a href="http://ww.dealerfire.com/" target="_blank">Automotive Marketing by DEALERFIRE</a>.</p>
+	</div>
 </footer>
 <a class="exit-off-canvas"></a>
 	
