d30cf543956441f727cd0379ed1dd668ff277068
Justin Friebel <friebj99@gmail.com>
2014-05-01 14:23:03 -0500

    remove featured post category from homepage posts

--- themes/foundation-dealer/index.php
+++ themes/foundation-dealer/index.php
@@ -1,23 +1,25 @@
 <?php get_header(); ?>
 
 	<div class="small-12 large-8 columns" role="main">
-	
-		<?php //query_posts( array ( 'category_name' => 'featured', 'posts_per_page' => 1 ) ); ?>
 		
-		<?php $my_query = new WP_Query('category_name=featured&posts_per_page=1'); ?>
+		<?php
+		
+			$featured_posts_query = new WP_Query('category_name=featured&posts_per_page=1');
 
-		<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
-		<?php endwhile; ?>
+			while ($featured_posts_query->have_posts()) : $featured_posts_query->the_post();
+				get_template_part( 'content', get_post_format() );
+			endwhile;
+			
+		?>
 	
 	<?php if ( have_posts() ) : ?>
 		
 		<?php do_action('foundationPress_before_content'); ?>
 		
-		
-	
 		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php if ( !in_category('3') ): ?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php endif; ?>
 		<?php endwhile; ?>
 		
 		<?php else : ?>
