5a3940bb7b426327a8ee38deed5e78b80989d840
Justin Friebel <friebj99@gmail.com>
2014-05-16 16:53:57 -0500

    missing target for social footer links

--- themes/foundation-dealer/footer.php
+++ themes/foundation-dealer/footer.php
@@ -4,16 +4,16 @@
 <footer id="footer">
   <div class="row">
     <div class="columns">
-    <a href="http://www.facebook.com/"><i class="fi-social-facebook social-icon"></i></a>
-    <a href="http://www.twitter.com/"><i class="fi-social-twitter social-icon"></i></a>
-    <a href="http://www.flickr.com/"><i class="fi-social-flickr social-icon"></i></a>
-    <a href="http://www.foursquare.com/"><i class="fi-social-foursquare social-icon"></i></a>
-    <a href="http://plus.google.com/"><i class="fi-social-google-plus social-icon"></i></a>
-    <a href="http://www.instagram.com/"><i class="fi-social-instagram social-icon"></i></a>
-    <a href="http://www.linkedin.com/"><i class="fi-social-linkedin social-icon"></i></a>
-    <a href="http://www.pinterest.com/"><i class="fi-social-pinterest social-icon"></i></a>
-    <a href="http://www.vimeo.com/"><i class="fi-social-vimeo social-icon"></i></a>
-    <a href="http://www.youtube.com/"><i class="fi-social-youtube social-icon"></i></a>
+    <a href="http://www.facebook.com/" target="_blank"><i class="fi-social-facebook social-icon"></i></a>
+    <a href="http://www.twitter.com/" target="_blank"><i class="fi-social-twitter social-icon"></i></a>
+    <a href="http://www.flickr.com/" target="_blank"><i class="fi-social-flickr social-icon"></i></a>
+    <a href="http://www.foursquare.com/" target="_blank"><i class="fi-social-foursquare social-icon"></i></a>
+    <a href="http://plus.google.com/" target="_blank"><i class="fi-social-google-plus social-icon"></i></a>
+    <a href="http://www.instagram.com/" target="_blank"><i class="fi-social-instagram social-icon"></i></a>
+    <a href="http://www.linkedin.com/" target="_blank"><i class="fi-social-linkedin social-icon"></i></a>
+    <a href="http://www.pinterest.com/" target="_blank"><i class="fi-social-pinterest social-icon"></i></a>
+    <a href="http://www.vimeo.com/" target="_blank"><i class="fi-social-vimeo social-icon"></i></a>
+    <a href="http://www.youtube.com/" target="_blank"><i class="fi-social-youtube social-icon"></i></a>
 		<?php do_action('foundationPress_before_footer'); ?>
 		<?php dynamic_sidebar("footer-widgets"); ?>
 		<?php do_action('foundationPress_after_footer'); ?>
