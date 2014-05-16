2d0050876039d4d3895eef19667ad3209b65f662
Justin Friebel <friebj99@gmail.com>
2014-05-16 15:50:23 -0500

    add image bg to hero area

--- themes/foundation-dealer/index.php
+++ themes/foundation-dealer/index.php
@@ -1,9 +1,4 @@
 <?php get_header(); ?>
-
-	<div class="hero">
-    <h1 class="home-heading small-12 row text-center"><?php bloginfo('name'); ?></h1>
-    <p class="text-center">Robbins Chevrolet has not become the longest operating Chevrolet dealer in the Houston area all on our own. Our family would like to thank every customer that has trusted us over the last 86 years to complete every job quickly, accurately, and fairly.</p>
-  </div>
 	
 	<div class="row">
 		<div class="small-12 large-8 columns" role="main">
